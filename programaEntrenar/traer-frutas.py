62976389221609898ea9baf96ec6e60324c72d90
tatymediina <tatimedinaiuni@gmail.com>
2025-05-28 16:14:49 -0300

    feat: :zap: Agregué los archivos module.json y algunas frutas de ejemplo

--- programaEntrenar/traer-frutas.py
+++ programaEntrenar/traer-frutas.py
@@ -5,4 +5,4 @@ frutas = ["manzana", "banana", "pera", "naranja", "uva"]
 
 for fruta in frutas:
     google_crawler = GoogleImageCrawler(storage={'root_dir': f'frutas/{fruta}'})
-    google_crawler.crawl(keyword=fruta, max_num=3, file_idx_offset=0)
+    google_crawler.crawl(keyword=fruta, max_num=2, file_idx_offset=0)
